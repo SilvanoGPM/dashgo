6f53fa1e5236f02fb58deb7ecd0041d081c95f32
SkyG0D <silvanosilvino@hotmail.com>
2022-06-17 04:30:20 -0300

    feat: add Pagination component

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,6 +18,7 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 
 import { Header } from 'components/Header';
 import { Sidebar } from 'components/Sidebar';
+import { Pagination } from 'components/Pagination';
 
 export default function UsersList() {
   return (
@@ -89,6 +90,8 @@ export default function UsersList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
