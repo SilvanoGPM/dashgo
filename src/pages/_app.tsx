fc3a308084a1fefa754743d319b23aef49add8d5
SkyG0D <silvanosilvino@hotmail.com>
2022-06-17 02:39:50 -0300

    feat: add basic config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,18 @@
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
+
+import { theme } from 'styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider theme={theme}>
       <Head>
         <title>Next Boilerplate</title>
-
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-
-        <meta name="description" content="Boilerplate for NextJS projects" />
-        <meta name="theme-color" content="#FFFFFF" />
       </Head>
 
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta name="description" content="Boilerplate for NextJS projects" />
+        <meta name="theme-color" content="#FFFFFF" />
+      </Head>
       <body>
         <Main />
         <NextScript />
