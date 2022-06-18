156b3a6b6fa48bc513c0bc0d7e5cc8b2c4a7e91d
SkyG0D <silvanosilvino@hotmail.com>
2022-06-18 18:04:56 -0300

    refactor: Dashboard responsive

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -75,7 +75,7 @@ export default function Dashboard() {
           minChildWidth="320px"
           alignItems="flex-start"
         >
-          <Box p="8" pb="4" bg="gray.800" borderRadius={8}>
+          <Box p={['6', '8']} pb="4" bg="gray.800" borderRadius={8}>
             <Text fontSize="large" mb="4">
               Inscritos da semana
             </Text>
@@ -83,7 +83,7 @@ export default function Dashboard() {
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
 
-          <Box p="8" pb="4" bg="gray.800" borderRadius={8}>
+          <Box p={['6', '8']} pb="4" bg="gray.800" borderRadius={8}>
             <Text fontSize="large" mb="4">
               Taxa de abertura
             </Text>
