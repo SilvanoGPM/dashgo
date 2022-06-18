989bf2c2130f9084c244f7faf1f70b9d81085338
SkyG0D <silvanosilvino@hotmail.com>
2022-06-18 18:14:42 -0300

    refactor: CreateUser page responsive

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -23,7 +23,7 @@ export default function CreateUser() {
       <Flex w="100%" my="6" mx="auto" px="6" maxWidth={1480}>
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Box flex="1" borderRadius={8} bg="gray.800" p={['6', '8']}>
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -31,12 +31,12 @@ export default function CreateUser() {
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" type="email" label="E-mail" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={['6', '8']} w="100%">
               <Input name="password" type="password" label="Senha" />
               <Input
                 name="password_confirmation"
@@ -47,7 +47,7 @@ export default function CreateUser() {
           </VStack>
 
           <Flex mt="8" justify="flex-end">
-            <HStack spacing="4">
+            <HStack spacing={['6', '8']}>
               <Link href="/users" passHref>
                 <Button colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
