5214e77a409d9a2ef51df3798eb96fff80ed6b50
SkyG0D <silvanosilvino@hotmail.com>
2022-06-20 19:15:15 -0300

    fix: invalid date fixed

--- src/hooks/api/useUsers.ts
+++ src/hooks/api/useUsers.ts
@@ -1,6 +1,13 @@
 import { useQuery } from 'react-query';
 import { api } from 'services/api';
 
+interface UserRaw {
+  id: string;
+  name: string;
+  email: string;
+  created_at: string;
+}
+
 interface User {
   id: string;
   name: string;
@@ -14,17 +21,17 @@ interface GetUsersResponse {
 }
 
 export async function getUsers(page: number): Promise<GetUsersResponse> {
-  const { data, headers } = await api.get<User[]>('/users', {
+  const { data, headers } = await api.get<UserRaw[]>('/users', {
     params: { page },
   });
 
   const totalCount = Number(headers['x-total-count']);
 
-  const users = data.map<User>(({ id, name, email, createdAt }) => ({
+  const users = data.map<User>(({ id, name, email, created_at }) => ({
     id,
     name,
     email,
-    createdAt: new Date(createdAt).toLocaleDateString('pt-BR', {
+    createdAt: new Date(created_at).toLocaleDateString('pt-BR', {
       day: '2-digit',
       month: 'long',
       year: 'numeric',
